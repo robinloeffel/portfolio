2c84ebce9553036401539b2cd9ac0bb3a3d32d73
Robin Löffel <hi@robinloeffel.ch>
2022-10-08 12:05:29 +0200

    deffo no more spam right

--- src/js/email-link.js
+++ src/js/email-link.js
@@ -1,11 +1,23 @@
 const spanToReplace = document.querySelector('[data-replace-with-link]');
+const dialog = document.querySelector('.human-question');
+const select = dialog.querySelector('select');
 const anchor = document.createElement('a');
 
 anchor.text = 'E-Mail';
 anchor.href = '';
-anchor.addEventListener('click', event => {
-  event.preventDefault();
-  window.location = [ ...'hc.leffeolnibor@ih:otliam' ].reverse().join('');
-});
 
 spanToReplace.replaceWith(anchor);
+
+anchor.addEventListener('click', event => {
+  event.preventDefault();
+  dialog.showModal();
+});
+
+select.addEventListener('change', ({ target }) => {
+  if (target.value === 'potatos') {
+    window.location = [ ...'hc.leffeolnibor@ih:otliam' ].reverse().join('');
+    select.value = '';
+  }
+
+  dialog.close();
+});
