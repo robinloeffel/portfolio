993c6702f6d9857c31793791547c6709d4020e68
Robin Löffel <hi@robinloeffel.ch>
2023-02-11 16:11:58 +0100

    refactor: simplify things

--- build.js
+++ build.js
@@ -1,88 +1,61 @@
-import { deleteAsync } from "del";
-import chokidar from "chokidar";
-import open from "open";
+import { exec } from "child_process";
+import { sassPlugin } from "esbuild-sass-plugin";
+import browserSync from "browser-sync";
 import esbuild from "esbuild";
 import eslint from "esbuild-plugin-eslint";
-import { sassPlugin } from "esbuild-sass-plugin";
 import postcss from "postcss";
 import env from "postcss-preset-env";
-import browserSync from "browser-sync";
 
-const watch = process.argv.includes("--watch");
+const config = (
+  sourcemap = false,
+  minify = false
+) => ({
+  entryPoints: [
+    "src/robin.ts",
+    "src/robin.scss"
+  ],
+  outdir: "public",
+  bundle: true,
+  plugins: [
+    eslint(),
+    sassPlugin({
+      async transform(source, directory) {
+        const processor = postcss([ env() ]);
+        const result = await processor.process(source, {
+          from: directory
+        });
+        return result.css;
+      }
+    })
+  ],
+  sourcemap,
+  minify
+});
 
-await deleteAsync("public/robin.*");
+exec("rm -rf public/robin.*", async () => {
+  if (process.argv.includes("--watch")) {
+    const context = await esbuild.context(config(true));
+    await context.watch();
 
-if (watch) {
-  const context = await esbuild.context({
-    entryPoints: [
-      "src/robin.ts",
-      "src/robin.scss"
-    ],
-    outdir: "public",
-    bundle: true,
-    sourcemap: true,
-    plugins: [
-      eslint(),
-      sassPlugin({
-        async transform(source, directory) {
-          const { css } = await postcss([
-            env()
-          ]).process(source, {
-            from: directory
-          });
-          return css;
-        }
-      })
-    ]
-  });
+    const bs = browserSync.create();
+    const watcher = bs.watch([
+      "src/**/*",
+      "public/*.{html,png,jpg,webp}"
+    ]);
 
-  const server = browserSync.create();
-  server.init({
-    host: "0.0.0.0",
-    notify: false,
-    open: true,
-    server: "public",
-    ui: false
-  });
+    const reload = () => bs.reload();
+    watcher.on("add", reload);
+    watcher.on("change", reload);
+    watcher.on("unlink", reload);
 
-  const watcher = chokidar.watch([
-    "src/**/*",
-    "public/index.html"
-  ]);
-  const callback = async () => {
-    await context.rebuild();
-    server.reload();
-  };
-
-  watcher.on("change", callback);
-  watcher.on("add", callback);
-  watcher.on("unlink", callback);
-} else {
-  await esbuild.build({
-    entryPoints: [
-      "src/robin.ts",
-      "src/robin.scss"
-    ],
-    outdir: "public",
-    bundle: true,
-    minify: true,
-    define: {
-      watchMode: "false"
-    },
-    plugins: [
-      eslint(),
-      sassPlugin({
-        async transform(source, directory) {
-          const { css } = await postcss([
-            env()
-          ]).process(source, {
-            from: directory
-          });
-          return css;
-        }
-      })
-    ]
-  });
-
-  await open("public");
-}
+    bs.init({
+      host: "0.0.0.0",
+      server: "public",
+      ui: false,
+      notify: false
+    });
+  } else {
+    await esbuild.build(config(false, true));
+    exec("open public");
+  }
+});
