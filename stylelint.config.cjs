5b8c12c33398820384490d7fc8209ddb58d07996
Robin Löffel <hi@robinloeffel.ch>
2023-07-28 15:17:53 +0200

    feat: add stylelint-stylistic

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -3,5 +3,15 @@ module.exports = {
   extends: [
     "stylelint-config-standard-scss",
     "stylelint-config-idiomatic-order"
-  ]
+  ],
+  plugins: [ "stylelint-stylistic" ],
+  rules: {
+    "stylistic/color-hex-case": "lower",
+    "stylistic/no-extra-semicolons": true,
+    "stylistic/number-leading-zero": "always",
+    "stylistic/number-no-trailing-zeros": true,
+    "stylistic/property-case": "lower",
+    "stylistic/string-quotes": "double",
+    "stylistic/unit-case": "lower"
+  }
 };
