d7ea21c337fda46d5d6d6976d94753c5ae49a9e1
Robin Löffel <hi@robinloeffel.ch>
2023-08-09 19:21:08 +0200

    refactor: rename esbuild.js to esbuild.config.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,4 @@
 /** @type {import("eslint").Linter.BaseConfig} */
 module.exports = {
-  extends: "sweet",
-  overrides: [{
-    files: "esbuild.js",
-    env: {
-      browser: false,
-      node: true
-    }
-  }]
+  extends: "sweet"
 };
