97803d56c472237249740fbed6523f2a0b2e2df6
Robin Löffel <hi@robinloeffel.ch>
2023-09-25 14:41:43 +0200

    refactor: tidy up styles

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -3,13 +3,7 @@ module.exports = {
   extends: [
     "stylelint-config-standard-scss",
     "stylelint-config-idiomatic-order",
-    "stylelint-config-html/html",
-    "stylelint-stylistic/config"
-  ],
-  rules: {
-    "selector-class-pattern": null,
-    "stylistic/indentation": null,
-    "stylistic/linebreaks": null,
-    "stylistic/max-line-length": null
-  }
+    "stylelint-stylistic/config",
+    "stylelint-config-html"
+  ]
 };
