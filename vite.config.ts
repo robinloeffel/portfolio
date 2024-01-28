05aa13f31a952f01bd3376b196dd24f142529c5e
Robin Löffel <hi@robinloeffel.ch>
2024-01-28 18:02:57 +0100

    fix: actually use browserslist in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import browserslist from "browserslist-to-esbuild";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -5,7 +6,8 @@ export default defineConfig({
 	build: {
 		outDir: "../dist",
 		emptyOutDir: true,
-		modulePreload: process.env.NODE_ENV === "development"
+		modulePreload: process.env.NODE_ENV === "development",
+		target: browserslist()
 	},
 	server: {
 		open: true
