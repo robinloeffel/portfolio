7ec55cce42cdda04d6e4d63b0f4691815b0fb2d6
Robin Löffel <hi@robinloeffel.ch>
2025-05-16 23:10:49 +0200

    refactor: add integrity to umami script, don't cloak umami

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,11 @@
 import sweet from "eslint-config-sweet";
-import { config } from "typescript-eslint";
 
-export default config(
-  sweet,
+/** @type {typeof sweet} */
+export default [
+  ...sweet,
   {
     rules: {
-      "github/no-dynamic-script-tag": 0,
-      "unicorn/prefer-dom-node-dataset": 0,
       "unicorn/prefer-global-this": 0
     }
   }
-);
+];
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,12 +3,3 @@ document.querySelector<HTMLAnchorElement>("[href='#email']")
     event.preventDefault();
     window.open(window.atob("bWFpbHRvOmhpQHJvYmlubG9lZmZlbC5jaA=="), "_self");
   });
-
-if (import.meta.env.PROD) {
-  const stats = document.createElement("script");
-  stats.src = "/stats.js";
-  stats.async = true;
-  stats.setAttribute("data-website-id", "d1008afe-4b86-4249-9980-fbcb8f500b20");
-  stats.setAttribute("data-domains", "robinloeffel.ch");
-  document.head.append(stats);
-}
