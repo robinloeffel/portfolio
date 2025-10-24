648002edc4c9c00604985a022f1a6994bb4e2e7c
Robin Löffel <6851646+robinloeffel@users.noreply.github.com>
2025-10-24 14:24:17 +0200

    refactor: simplify

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,4 @@
 import sweet from "eslint-config-sweet";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig(
-  sweet,
-  {
-    rules: {
-      "unicorn/prefer-global-this": 0
-    }
-  }
-);
+export default defineConfig(sweet);
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,23 +1,21 @@
 const nav = document.querySelector<HTMLElement>("footer nav");
-const isSus = Object.values({
-  hasWebDriver: navigator.webdriver,
-  hasHeadlessInUserAgent: navigator.userAgent.toLowerCase().includes("headless"),
-  hasNoUserLanguages: navigator.languages.length === 0,
-  hasNoWindowDimensions: !window.innerWidth || !window.innerHeight,
-  hasDisallowedAttributes: new Set(["selenium", "webdriver", "driver", "playwright"])
-    .intersection(new Set(document.documentElement.getAttributeNames()))
-    .size > 0
-}).some(Boolean);
-const handleClick = (event: MouseEvent) => {
-  event.preventDefault();
-  window.location.href = window.atob("bWFpbHRvOmhpQHJvYmlubG9lZmZlbC5jaA==");
-};
+const sus = navigator.webdriver
+  || navigator.userAgent.toLowerCase().includes("headless")
+  || navigator.languages.length === 0
+  || !window.innerWidth
+  || !window.innerHeight;
 
-if (nav && !isSus) {
+if (nav && !sus) {
   const anchor = document.createElement("a");
+
   anchor.href = "#!";
   anchor.dataset.umamiEvent = "email-link";
   anchor.setAttribute("aria-label", "Email");
-  anchor.addEventListener("click", handleClick);
+
+  anchor.addEventListener("click", (event: MouseEvent) => {
+    event.preventDefault();
+    globalThis.location.href = globalThis.atob("bWFpbHRvOmhpQHJvYmlubG9lZmZlbC5jaA==");
+  });
+
   nav.prepend(anchor);
 }
